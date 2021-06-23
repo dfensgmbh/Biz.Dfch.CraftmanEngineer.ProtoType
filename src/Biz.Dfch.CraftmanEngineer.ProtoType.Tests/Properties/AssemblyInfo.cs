f6aa9c354c83cfde65e728b7cb5229cc331eee3b
Ronald Rink <ronald.rink@d-fens.net>
2021-06-23 18:28:44 +0200

    initial commit
     * project created

---
+++ src/Biz.Dfch.CraftmanEngineer.ProtoType.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Biz.Dfch.CraftmanEngineer.ProtoType.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Biz.Dfch.CraftmanEngineer.ProtoType.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cd2cfff0-12d1-42cf-9304-a54bce0faf76")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
